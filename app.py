66e753fea5a305c9881a698db52f2591ca5af71f
clutch2227 <garyj@udel.edu>
2021-02-21 13:19:07 -0500

    added the word another

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-  return 'Flask is running test'
+  return 'Flask is running another test'
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
