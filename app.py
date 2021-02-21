9931fe0450a9d4156e5be93b5ddf24d0fa43ac7d
clutch2227 <garyj@udel.edu>
2021-02-21 17:09:09 -0500

    feature commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-  return 'Flask is running another test'
+  return 'Flask is running another test using git'
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
